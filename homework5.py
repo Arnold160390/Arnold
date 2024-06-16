c131ff494bcc0c3c948bf3be8a8397235bdfb6f1
Arnold160390 <arni_909090@mail.ru>
2024-06-16 14:30:09 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 'name', 1990, 52.2
+print(immutable_var)
+#immutable_var[0] = '16' # кортеж невозможно изменить, это его особенность в отличии от списка
+mutable_list = [12, 1991, 70.1]
+mutable_list[2] = 13
+print(mutable_list)
