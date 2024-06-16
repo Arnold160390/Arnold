85eddcb083f3c8111b3bff07aa641c1ed3ee56f4
Arnold160390 <arni_909090@mail.ru>
2024-06-16 13:36:51 +0300

    Update homework44.py

--- homework44.py
+++ homework44.py
@@ -1,8 +1,1 @@
-my_string = input('Привет, меня зовут Арнольд!')
-a='Привет, меня зовут Арнольд!'
-print(len(a))
-print(a.upper())
-print(a.lower())
-print(a.replace(' ', ''))
-print(a[0])
-print(a[-1])
+
