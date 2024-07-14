4bd9fee26af76be30331e2ad53c86be5ab255ff5
Arnold160390 <arni_909090@mail.ru>
2024-07-14 14:49:33 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Arnold': 1990, 'Alina': 2002, 'Vladimir': 1992}
+print(my_dict)
+print(my_dict['Arnold'])
+print(my_dict.get('Oleg'))
+my_dict.update({'Anna': 1993, 'Sasha': 1996})
+my_dict['Alina']
+a = my_dict.pop('Alina')
+print(a)
+print(my_dict)
+
+my_set = {'Name', 2024, 13.4, 'Name', 2024}
+print (my_set)
+my_set.update({'Anna', 1945})
+my_set.discard(2024)
+print(my_set)
